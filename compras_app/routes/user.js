dc2d9636e454c079e6d15b266130e2a2a3c72d57
Hans Alvarez F <alvarez.hans@gmail.com>
2022-06-29 01:38:43 -0400

    primera subida, CRUD usuarios OK

---
+++ compras_app/routes/user.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+// --
+const { sequelize, usuarios } = require('../models');
+
+/* CREATE functions. */
+router.get('/create', async function(req, res, next) {
+  res.render('usuario/create')
+});
+
+router.post('/create', async (req, res, next) => {
+    console.log(req.body);
+
+    const { nombre, usuario, password } = req.body;
+
+    try {
+      const _usuario = await usuarios.create({ nombre, usuario, password });
+      console.debug('Usuario creado sin problemas.', { _usuario });
+      res.redirect('/users');
+    } catch (error) {
+      // TODO: Pendiente de generar error
+      console.error({ error });
+    }
+  });
+
+/* UPDATE functions. */
+router.get('/update/:id', async function(req, res, next) {
+  console.log(req.params);
+
+  const { id } = req.params;
+
+  const _usuario = await usuarios.findByPk( id );
+  res.render('usuario/update', { _usuario });
+});
+
+
+router.post('/update/:id', async function(req, res, next) {
+  console.log('req.params', req.params);
+  console.log('req.body', req.body);
+
+  const { id } = req.params;
+  const { nombre, usuario, password } = req.body;
+
+  const _usuario = await usuarios.findByPk( id );
+  _usuario.nombre = nombre;
+  _usuario.usuario = usuario;
+  _usuario.password = password;
+
+  try {
+    await _usuario.save();
+    console.debug('Usuario actualizado sin problemas.', { _usuario });
+    res.redirect('/users');
+  } catch (error) {
+    // TODO: Pendiente de generar error
+    console.error({ error });
+  }
+});
+
+/* DELETE functions. */
+router.get('/delete/:id', async function(req, res, next) {
+  console.log(req.params);
+
+  const { id } = req.params;
+
+  const _usuario = await usuarios.findByPk( id );
+  try {
+    await _usuario.destroy();
+    console.debug('Usuario actualizado sin problemas.', { _usuario });
+    res.redirect('/users');
+  } catch (error) {
+    // TODO: Pendiente de generar error
+    console.error({ error });
+  }  
+});
+
+
+module.exports = router;
---
+++ compras_app/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+// --
+const { sequelize, usuarios } = require('../models');
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  const _usuarios = await usuarios.findAll();
+  console.debug('get usuarios', {_usuarios});
+  res.render('usuarios/usuarios', { _usuarios });
+  // res.render('usuarios/usuarios')
+});
+
+
+module.exports = router;
